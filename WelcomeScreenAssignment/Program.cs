51f02a952363f44626542af32efbb2c09dc086c0
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-01-24 10:18:08 -0800

    C# beginnings

---
+++ WelcomeScreenAssignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
