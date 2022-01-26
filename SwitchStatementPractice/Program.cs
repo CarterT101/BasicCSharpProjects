301ce42502c04af3b81759a19c431fd24b8d7edf
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-01-25 18:31:59 -0800

    While statement practice

---
+++ SwitchStatementPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatementPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number 1-3:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isGuessed = false;
+
+            while (!isGuessed)
+            {
+
+            }
+            Console.Read();
+
+        }
+    }
+}
