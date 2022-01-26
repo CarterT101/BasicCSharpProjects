671a7b49a777f466d307b5089dea0be15ca2f41f
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-01-26 13:37:55 -0800

    string practice

---
+++ StringPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello, my name is ";
+            string name = "Carter, and I ";
+            string sentence = "like software development.";
+            string sentenceUpper = sentence.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(hello + name + sentenceUpper);
+            Console.WriteLine(sb);
+            Console.Read();
+        }
+    }
+}
