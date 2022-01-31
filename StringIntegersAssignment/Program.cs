de4542afe64fb6d0de0ce3e808274c0bbf98bc91
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-01-31 12:06:56 -0800

    Exceptions practice and assignments

---
+++ StringIntegersAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringIntegersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int>() { 10, 4, 6, 9, 7, 3 };
+            foreach (int n in numberList)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("Input a number to divide each number in the list by");
+            int yourNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Read();
+        }
+    }
+}
