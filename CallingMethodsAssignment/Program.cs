4cf3f8a2d27e2cbf5da2ad097aaf754e2b30f919
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-02-01 15:57:35 -0800

    Calling methods assignment

--- CallingMethodsAssignment/Program.cs
+++ CallingMethodsAssignment/Program.cs
@@ -8,8 +8,35 @@ namespace CallingMethodsAssignment
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Integers add = new Integers();
+
+            Console.WriteLine("Input a number to add 10 to it");
+            int yourNum = Convert.ToInt32(Console.ReadLine());
+            int result = add.Add(yourNum); //instance of Integers called add, passed in number 
+            Console.WriteLine(result);
+
+
+            Console.WriteLine("Input a number to subtract 5 from it");
+            int subNum = Convert.ToInt32(Console.ReadLine());
+            result = add.Subtract(subNum);
+            Console.WriteLine(result);
+
+
+            Console.WriteLine("Input a number to be multiplied by 3");
+            int multNum = Convert.ToInt32(Console.ReadLine());
+            result = add.Multiply(multNum);
+            Console.WriteLine(result);
+
+
+
+
+
+
+
+            Console.Read();
+
         }
     }
 }
