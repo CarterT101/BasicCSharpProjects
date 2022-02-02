85dd5d566cac249c11ab8d07c532b634f6857601
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-02-02 01:38:27 -0800

    method assignment 2

---
+++ MethodAssignment2/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment2
+{
+    public class Operation
+    {
+        public void Multi(int mult, int mult1)
+        {
+            int answer = mult + 5;
+            Console.WriteLine(mult1);
+        }
+    }
+}
---
+++ MethodAssignment2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation oper = new Operation();
+
+            int result = oper.Multi(10, 5);
+
+            result = oper.Multi(mult: 10, mult1: 5);
+        }
+    }
+}
