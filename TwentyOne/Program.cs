71c05c614bfc29fa5ed2695ca1eb9ca653a6ce37
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-02-01 16:50:26 -0800

    method practice

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -12,35 +12,51 @@ namespace TwentyOne
         {
 
             Deck deck = new Deck(); //deck object that has property cards should have 52 cards
-            deck = Shuffle(deck);
+            int timesShuffled = 0;
+            deck = Shuffle(deck, out timesShuffled, 3);
+            //deck = Shuffle(deck: deck, times: 3); //named parameter
 
-            
+
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
 
             Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times Shuffled: {0}", timesShuffled);
 
             Console.Read();
         }
 
-        public static Deck Shuffle(Deck deck) //public (accessible anywhere), static (dont want to create object program before calling), deck (type of data returning),
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1) //public (accessible anywhere), static (dont want to create object program before calling), deck (type of data returning),
                                               //shuffle (name of function), parameter of type 'Deck' and variable name 'deck' when you want to refer to it inside method
         {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random(); //object 'random' has some methods associated with it
-
-            while (deck.Cards.Count > 0)
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
             {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex); 
-            }
-            deck.Cards = TempList;
-            return deck;
+                timesShuffled++;
+                List<Card> TempList = new List<Card>();
+                Random random = new Random(); //object 'random' has some methods associated with it
 
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = TempList;
+            }
+            return deck;
         }
+
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
 
     }
 }
