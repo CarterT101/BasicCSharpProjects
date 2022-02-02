674e671e5aa4c124fe4a580e85c4300d59c1e899
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-02-01 17:50:23 -0800

    assignment

---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation oper = new Operation(); //instance
+
+            Console.WriteLine("Input a number");
+            int yourNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a second number if you'd like");
+            int yourNum1 = Convert.ToInt32(Console.ReadLine());
+            int result = oper.Multi(mult: yourNum, mult1: yourNum1);
+            Console.WriteLine(result);
+
+
+
+            Console.Read();
+        }
+    }
+}
