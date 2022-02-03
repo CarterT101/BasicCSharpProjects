399550916964c014eaba28a025ef3bc9495589cb
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-02-03 11:05:19 -0800

    method object practice inheritance

--- MethodObjectAssignment/Person.cs
+++ MethodObjectAssignment/Person.cs
@@ -8,5 +8,12 @@ namespace MethodObjectAssignment
 {
     class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
     }
 }
--- MethodObjectAssignment/Program.cs
+++ MethodObjectAssignment/Program.cs
@@ -10,6 +10,15 @@ namespace MethodObjectAssignment
     {
         static void Main(string[] args)
         {
+            Employee empl = new Employee();
+
+            empl.FirstName = "Sample";
+            empl.LastName = "Student";
+
+            empl.SayName();
+
+            Console.Read();
+
         }
     }
 }
