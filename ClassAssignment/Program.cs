0a932e9995875b67083c5db2da7c790515cf1d13
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-02-02 16:46:11 -0800

    updated

--- ClassAssignment/Program.cs
+++ ClassAssignment/Program.cs
@@ -15,21 +15,21 @@ namespace ClassAssignment
             Console.WriteLine("Input a number to be divided by 2");
             int yourNum = Convert.ToInt32(Console.ReadLine());
 
-            oper.Div(yourNum);
+            oper.Div(yourNum); //calling first method which has a WriteLine inside it
 
-            oper.Mult(yourNum);
+            oper.Mult(yourNum); //calling second method that also has WriteLine in it
 
 
             Console.WriteLine("Input second number");
             int yourNum1 = Convert.ToInt32(Console.ReadLine());
-            int result = oper.Mult(yourNum, yourNum1);
+            int result = oper.Mult(yourNum, yourNum1); //Overloaded method and printing it next line
 
             Console.WriteLine("This is your result if the first inputted number multiplied by second inputted number:\n{0}", result);
 
             Console.WriteLine("Input another number");
             yourNum = Convert.ToInt32(Console.ReadLine());
 
-            result = Operation.Times(yourNum);
+            result = Operation.Times(yourNum); //static method calling 
             Console.WriteLine("This is your number times 10:\n{0}", result);
 
             Console.Read();
