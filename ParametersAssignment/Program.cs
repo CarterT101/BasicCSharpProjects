b7863bd2abd5f081f672ff22ae6bf59fd896e731
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-02-03 18:13:09 -0800

    parameter assignment

---
+++ ParametersAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> empl = new Employee<string>();
+            empl.things = "Hello";
+
+            Console.WriteLine(empl);
+            Console.Read();
+        }
+    }
+}
