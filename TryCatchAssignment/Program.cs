5d40ee458fca5384ed75137726f37c628a45174e
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-02-07 16:23:55 -0800

    yourage in datetime

--- TryCatchAssignment/Program.cs
+++ TryCatchAssignment/Program.cs
@@ -10,6 +10,33 @@ namespace TryCatchAssignment
     {
         static void Main(string[] args)
         {
+            bool validAnswer = false;
+            int yourAge = 1;
+            while (!validAnswer)
+            {
+                try
+                {
+                    Console.WriteLine("What is your age");
+                    validAnswer = int.TryParse(Console.ReadLine(), out yourAge);
+                    if (yourAge <= 0)
+                    {
+                        Console.Write("Impossible");
+                        Console.Read();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Enter valid age");
+                    Console.Read();
+                    return;
+                }
+
+            }
+            DateTime today = DateTime.Today;
+            Console.WriteLine(today);
+            DateTime yourDate = today.AddYears(-yourAge);
+            Console.WriteLine("You were born in {0}", yourDate);
+            Console.Read();
         }
     }
 }
