d99b3be6f7980414160712681d75c3aae278eaa4
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-02-15 17:43:28 -0800

    student assignment

---
+++ FinalChallengeAssignment/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalChallengeAssignment
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Grade Grade { get; set; }
+    }
+}
---
+++ StudentAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace StudentAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            using (var db = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Carter" };
+                db.Students.Add(stud);
+                db.SaveChanges();
+            }
+            Console.Read();
+        }
+    }
+}
